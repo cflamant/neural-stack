94a19a9d191b7ab186c5247328bb527ee6df0a4c
cflamant <cflamant@gmail.com>
2019-10-21 01:11:46 -0400

    add a bash script to run rnn 1 layer

---
+++ rnn_1l.sh
@@ -0,0 +1,6 @@
+python rnn_256h_1l_50b_Adam_1e-3.py
+python rnn_256h_1l_50b_Adam_1e-4.py
+python rnn_256h_1l_50b_RMSprop_1e-3.py
+python rnn_256h_1l_50b_RMSprop_1e-4.py
+python rnn_256h_1l_50b_SGD_1e-3.py
+python rnn_256h_1l_50b_SGD_1e-4.py
